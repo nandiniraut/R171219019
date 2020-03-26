375c8f479fe7c27f9cd0b546095dfe090ab95aef
Nandini Raut <rautnandini2001@gmail.com>
2020-03-26 21:37:48 +0530

    pointers2.cpp

--- pointers2.cpp
+++ pointers2.cpp
@@ -8,4 +8,10 @@ int main()
 	b= 20;
 	p1=&a;
 	p2=&b;
-	
+cout<<"the addresses of variables a and b are "<<" "<<p1<<" "<<p2<<endl;
+	cout<<"the values stored in a nd b are"<<" "<<a<<" "<<b<<endl;
+	*p1=*p2;
+	*p2=8;
+        cout<<"new values of a and b are"<<" "<<a<<" "<<b<<endl;
+return 0;
+}	
