d5c90dfaccdaf06af726b62adfa301a523767eaf
Nandini Raut <rautnandini2001@gmail.com>
2020-03-26 21:27:48 +0530

    output code

--- pointer.cpp
+++ pointer.cpp
@@ -14,6 +14,12 @@ int main()
   foo =&obj;
   bar=new rectangle(10,15);
   baz=new rectangle[2]{{2,3},{4,6}};
+cout<<"objs area:"<<obj.area()<<endl;
+  cout<<"*foos area:"<<* foo->area()<<endl;
+  cout<<"*bars area:"<<* bar->area()<<endl;
+  cout<<"*baz[0]s area"<<* baz[0].area()<<endl;
+  cout<<"*baz[1]s area:"<<* baz[1].area()<<endl;
+  delete bar;
+  delete []baz;
   return 0;
-}
-
+  }
