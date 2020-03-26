ac560cc4a527cd883c19bbda34fd593f7b317085
Nandini Raut <rautnandini2001@gmail.com>
2020-03-26 21:20:42 +0530

    declaring objects and pointer

--- pointer.cpp
+++ pointer.cpp
@@ -7,4 +7,13 @@ class rectangle
              rectangle(int x,int y):height(x),width(y){}
 	     int area(void){ return height*width; }
 }; 
+int main()
+{
+  rectangle obj(3,5);
+  rectangle * foo,* bar,* baz;
+  foo =&obj;
+  bar=new rectangle(10,15);
+  baz=new rectangle[2]{{2,3},{4,6}};
+  return 0;
+}
 
